66d17ecb879aac82bf36648d870c1fc0b3c49332
Ubuntu <ubuntu@puppet.stratos.com>
2014-03-06 18:00:22 +0530

    removing file templace backup

--- clean.sh
+++ clean.sh
@@ -23,25 +23,25 @@ do
   esac
 done
 
-if [[  -f /etc/puppet/manifests/nodes.pp.orig ]]; then
-    echo "Backing up the original nodes.pp file"
-    cp -f /etc/puppet/manifests/nodes.pp.orig /etc/puppet/manifests/nodes.pp
-fi
-
-if [[  -f /etc/puppet/modules/appserver/manifests/params.pp.orig ]]; then
-    echo "Backing up the original AS params.pp file"
-    cp -f /etc/puppet/modules/appserver/manifests/params.pp.orig /etc/puppet/modules/appserver/manifests/params.pp
-fi
-
-if [[  -f /etc/puppet/modules/esb/manifests/params.pp.orig ]]; then
-    echo "Backing up the original ESB params.pp file"
-    cp -f /etc/puppet/modules/esb/manifests/params.pp.orig /etc/puppet/modules/esb/manifests/params.pp
-fi
-
-if [[  -f stratos-installer/conf/setup.conf.orig ]]; then
-    echo "Backing up the original setup.conf file"
-    cp -f stratos-installer/conf/setup.conf.orig stratos-installer/conf/setup.conf
-fi
+#if [[  -f /etc/puppet/manifests/nodes.pp.orig ]]; then
+#    echo "Backing up the original nodes.pp file"
+#    cp -f /etc/puppet/manifests/nodes.pp.orig /etc/puppet/manifests/nodes.pp
+#fi
+#
+#if [[  -f /etc/puppet/modules/appserver/manifests/params.pp.orig ]]; then
+#    echo "Backing up the original AS params.pp file"
+#    cp -f /etc/puppet/modules/appserver/manifests/params.pp.orig /etc/puppet/modules/appserver/manifests/params.pp
+#fi
+#
+#if [[  -f /etc/puppet/modules/esb/manifests/params.pp.orig ]]; then
+#    echo "Backing up the original ESB params.pp file"
+#    cp -f /etc/puppet/modules/esb/manifests/params.pp.orig /etc/puppet/modules/esb/manifests/params.pp
+#fi
+#
+#if [[  -f stratos-installer/conf/setup.conf.orig ]]; then
+#    echo "Backing up the original setup.conf file"
+#    cp -f stratos-installer/conf/setup.conf.orig stratos-installer/conf/setup.conf
+#fi
 
 #mysql -u $mysql_user -p$mysql_pass -e "DROP DATABASE IF EXISTS registry;"
 
