91bcab0f37e20515982783b457d06a62ffdf3543
Melan Nimesh <melan@apache.org>
2014-04-24 23:00:21 +0530

    Fixing appserver download cmd

--- puppet/modules/appserver/manifests/initialize.pp
+++ puppet/modules/appserver/manifests/initialize.pp
@@ -17,6 +17,13 @@
 # Initializing the deployment
 
 define appserver::initialize ($repo, $version, $service, $local_dir, $target, $mode, $owner,) {
+
+  file {
+    "/${local_dir}/wso2${service}-${version}.zip":
+      ensure => present,
+      source => "puppet:///modules/appserver/wso2${service}-${version}.zip";
+    }
+
   exec {
     "creating_target_for_${name}":
       path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
@@ -31,7 +38,7 @@ define appserver::initialize ($repo, $version, $service, $local_dir, $target, $m
       path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
       cwd       => $local_dir,
       unless    => "test -f ${local_dir}/wso2${service}-${version}.zip",
-      command   => "wget -q ${repo}/wso2${service}-${version}.zip",
+      command   => "puppet:///modules/appserver/wso2${service}-${version}.zip",
       logoutput => 'on_failure',
       creates   => "${local_dir}/wso2${service}-${version}.zip",
       timeout   => 0,
