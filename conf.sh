df32d985c14a6ea5fca8ebbd37cbe74587b9d937
Manula Thantriwatte <manula@apache.org>
2014-05-06 05:02:41 +0000

    Comment add to conf.sh file

--- conf.sh
+++ conf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export ACTIVE_MQ_DISTRIBUTION=apache-activemq-5.9.1-bin.tar.gz # Relavent activemq distribution
-export ACTIVE_MQ_EXTRACTED=apache-activemq-5.9.1
+export ACTIVE_MQ_EXTRACTED=apache-activemq-5.9.1 # Extracted activemq distribution folder name
 
 export JAVA_FILE_DISTRUBUTION=jdk-7u51-linux-x64.tar.gz # Relevant JDK distribution
-export JAVA_NAME_EXTRACTED=jdk1.7.0_51
+export JAVA_NAME_EXTRACTED=jdk1.7.0_51 # Extracted JDK folder name
 
 export MYSQL_CONNECTOR=mysql-connector-java-5.1.29-bin.jar # Relevant MySQL connector 
 
