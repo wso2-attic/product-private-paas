ca6a4630b8c0a0e9861d9205e381fda0b5f50f31
Manula Thantriwatte <manula@apache.org>
2014-05-22 06:19:54 +0000

    Edit gitblit.sh

--- stratos-installer/gitblit.sh
+++ stratos-installer/gitblit.sh
@@ -5,15 +5,13 @@ source "./conf/setup.conf"
 
 # Setting GitBlit
 mkdir $gitblit_path
-
 tar xzf $gitblit_pack_path -C $gitblit_path
-
 cp -f ./config/gitblit/gitblit.properties $gitblit_path/data
 
 pushd $gitblit_path
 
 echo "Starting Gitblit Server ..." >> $LOG
-nohup $gitblit_path/gitblit.sh &
+nohup $JAVA_HOME/bin/java -jar gitblit.jar --baseFolder data &
 echo "Gitblit server started" >> $LOG
 
 popd
