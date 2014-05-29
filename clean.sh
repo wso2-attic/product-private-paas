d67ca50c2445540bcb59c13af36c755d6c390534
sajhak <sajhak@gmail.com>
2014-05-29 14:23:27 +0000

    Fixing issue in clean.sh script

--- clean.sh
+++ clean.sh
@@ -110,14 +110,15 @@ if [[ $clean_puppet = "true" ]]; then
    sudo /etc/init.d/puppetmaster restart
 fi
 
+
 if [[ -z $clean_install ]]; then
    read -p "Do you want to clean Private PaaS Install Directory in $stratos_install_path? [y/n] " input_clean_install
-   if [[ $iinput_clean_install =~ ^[Yy]$ ]]; then
+   if [[ $input_clean_install =~ ^[Yy]$ ]]; then
       rm -rf $stratos_install_path/*
+      echo "Private PaaS install directory cleaned up !"
    else
       echo "Private PaaS install directory didn't clear"
    fi
 fi
 
-echo -e "\nSuccessfully cleaned up everything!"
 # END
