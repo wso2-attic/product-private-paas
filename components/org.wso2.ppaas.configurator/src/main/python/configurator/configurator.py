ac5f97fa5f01685ec68334383a70493d5f61d458
Imesh Gunaratne <imesh@wso2.com>
2015-05-26 18:34:48 +0530

    Making configurator an executable

--- components/org.wso2.ppaas.configurator/src/main/python/configurator/configurator.py
+++ components/org.wso2.ppaas.configurator/src/main/python/configurator/configurator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,7 +17,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-# !/usr/bin/env python
 import ast
 import logging
 import logging.config
