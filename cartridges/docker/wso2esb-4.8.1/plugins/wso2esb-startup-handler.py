7f5f387aa0f7074be7dbc412689813f5bd0571c0
Imesh Gunaratne <imesh@wso2.com>
2015-05-29 01:59:32 +0530

    Updating wso2/esb:4.8.1 docker image to set https proxy port

--- cartridges/docker/wso2esb-4.8.1/plugins/wso2esb-startup-handler.py
+++ cartridges/docker/wso2esb-4.8.1/plugins/wso2esb-startup-handler.py
@@ -29,6 +29,34 @@ class WSO2ESBStartupHandler(ICartridgeAgentPlugin):
     def run_plugin(self, values):
         log = LogFactory().get_log(__name__)
 
+        # read kubernetes service https port
+        log.info("Reading port mappings...")
+        port_mappings_str = values["PORT_MAPPINGS"]
+        https_port = None
+
+        # port mappings format: "PROTOCOL:http|PORT:80|PROXY_PORT:8280,PROTOCOL:https|PORT:773|PROXY_PORT:9443"
+        log.info("Port mappings: %s" % port_mappings_str)
+        if port_mappings_str is not None:
+
+            port_mappings_array = port_mappings_str.split(",")
+            if port_mappings_array:
+
+                for port_mapping in port_mappings_array:
+                    log.debug("port_mapping: %s" % port_mapping)
+                    name_value_array = port_mapping.split("|")
+                    log.debug("name_value_array: %s" % name_value_array)
+                    protocol = name_value_array[0].split(":")[1]
+                    port = name_value_array[1].split(":")[1]
+                    if protocol == "https":
+                        https_port = port
+
+        log.info("Kubernetes service https port: %s" % https_port)
+        if https_port is not None:
+            catalina_replace_command = "sed -i \"s/^STRATOS_HTTPS_PROXY_PORT = .*/STRATOS_HTTPS_PROXY_PORT = %s/g\" %s" % (https_port, "${CONFIGURATOR_HOME}/templates/wso2esb-4.8.1/configs.ini")
+            p = subprocess.Popen(catalina_replace_command, shell=True)
+            output, errors = p.communicate()
+            log.info("Successfully updated https proxy port in ESB template module")
+
         # configure server
         log.info("Configuring WSO2 ESB...")
         config_command = "exec /opt/wso2configurator-4.1.0/configurator.py"
@@ -44,5 +72,4 @@ class WSO2ESBStartupHandler(ICartridgeAgentPlugin):
         env_var = os.environ.copy()
         p = subprocess.Popen(start_command, env=env_var, shell=True)
         output, errors = p.communicate()
-
         log.debug("WSO2 ESB started successfully")
