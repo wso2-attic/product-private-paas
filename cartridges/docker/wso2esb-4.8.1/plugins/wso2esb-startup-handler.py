5ded5adbc7f15edf5ccd389a9a0b47a05afd91db
Imesh Gunaratne <imesh@wso2.com>
2015-05-31 09:49:46 +0100

    Adding logic to configure pass-through http and https ports in the esb docker cartridge

--- cartridges/docker/wso2esb-4.8.1/plugins/wso2esb-startup-handler.py
+++ cartridges/docker/wso2esb-4.8.1/plugins/wso2esb-startup-handler.py
@@ -29,33 +29,57 @@ class WSO2ESBStartupHandler(ICartridgeAgentPlugin):
     def run_plugin(self, values):
         log = LogFactory().get_log(__name__)
 
-        # read kubernetes service https port
         log.info("Reading port mappings...")
         port_mappings_str = values["PORT_MAPPINGS"]
-        https_port = None
 
-        # port mappings format: "PROTOCOL:http|PORT:80|PROXY_PORT:8280,PROTOCOL:https|PORT:773|PROXY_PORT:9443"
+        mgt_console_https_port = None
+        pt_http_port = None
+        pt_https_port = None
+
+        # port mappings format: """NAME:mgt-console|PROTOCOL:https|PORT:4500|PROXY_PORT:8443;
+        #                          NAME:pt-http|PROTOCOL:http|PORT:4501|PROXY_PORT:7280;
+        #                          NAME:pt-https|PROTOCOL:https|PORT:4502|PROXY_PORT:7243"""
+
         log.info("Port mappings: %s" % port_mappings_str)
         if port_mappings_str is not None:
 
-            port_mappings_array = port_mappings_str.split(",")
+            port_mappings_array = port_mappings_str.split(";")
             if port_mappings_array:
 
                 for port_mapping in port_mappings_array:
                     log.debug("port_mapping: %s" % port_mapping)
                     name_value_array = port_mapping.split("|")
-                    log.debug("name_value_array: %s" % name_value_array)
-                    protocol = name_value_array[0].split(":")[1]
-                    port = name_value_array[1].split(":")[1]
-                    if protocol == "https":
-                        https_port = port
+                    name = name_value_array[0].split(":")[1]
+                    protocol = name_value_array[1].split(":")[1]
+                    port = name_value_array[2].split(":")[1]
+                    if name == "mgt-console" and protocol == "https":
+                        mgt_console_https_port = port
+                    if name == "pt-http" and protocol == "http":
+                        pt_http_port = port
+                    if name == "pt-https" and protocol == "https":
+                        pt_https_port = port
 
-        log.info("Kubernetes service https port: %s" % https_port)
-        if https_port is not None:
-            catalina_replace_command = "sed -i \"s/^STRATOS_HTTPS_PROXY_PORT = .*/STRATOS_HTTPS_PROXY_PORT = %s/g\" %s" % (https_port, "${CONFIGURATOR_HOME}/templates/wso2esb-4.8.1/configs.ini")
-            p = subprocess.Popen(catalina_replace_command, shell=True)
+        log.info("Kubernetes service management console https port: %s" % mgt_console_https_port)
+        log.info("Kubernetes service pass-through http port: %s" % pt_http_port)
+        log.info("Kubernetes service pass-through https port: %s" % pt_https_port)
+
+        if mgt_console_https_port is not None:
+            command = "sed -i \"s/^CONFIG_PARAM_HTTPS_PROXY_PORT = .*/CONFIG_PARAM_HTTPS_PROXY_PORT = %s/g\" %s" % (mgt_console_https_port, "${CONFIGURATOR_HOME}/templates/wso2esb-4.8.1/configs.ini")
+            p = subprocess.Popen(command, shell=True)
             output, errors = p.communicate()
-            log.info("Successfully updated https proxy port in ESB template module")
+            log.info("Successfully updated management console https proxy port: %s in ESB template module" % mgt_console_https_port)
+
+        if pt_http_port is not None:
+            command = "sed -i \"s/^CONFIG_PARAM_PT_HTTP_PROXY_PORT = .*/CONFIG_PARAM_PT_HTTP_PROXY_PORT = %s/g\" %s" % (pt_http_port, "${CONFIGURATOR_HOME}/templates/wso2esb-4.8.1/configs.ini")
+            p = subprocess.Popen(command, shell=True)
+            output, errors = p.communicate()
+            log.info("Successfully updated pass-through http proxy port: %s in ESB template module" % pt_http_port)
+
+        if pt_https_port is not None:
+            command = "sed -i \"s/^CONFIG_PARAM_PT_HTTPS_PROXY_PORT = .*/CONFIG_PARAM_PT_HTTPS_PROXY_PORT = %s/g\" %s" % (pt_https_port, "${CONFIGURATOR_HOME}/templates/wso2esb-4.8.1/configs.ini")
+            p = subprocess.Popen(command, shell=True)
+            output, errors = p.communicate()
+            log.info("Successfully updated pass-through https proxy port: %s in ESB template module" % pt_https_port)
 
         # configure server
         log.info("Configuring WSO2 ESB...")
