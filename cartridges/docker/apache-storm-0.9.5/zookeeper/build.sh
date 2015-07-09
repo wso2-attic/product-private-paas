52ab30971c549971d4823545bb7ad8ea286e7a3a
reka <rthirunavukkarasu23@gmail.com>
2015-07-09 22:21:12 +0530

    zookeeper and storm ui

--- cartridges/docker/apache-storm-0.9.5/zookeeper/build.sh
+++ cartridges/docker/apache-storm-0.9.5/zookeeper/build.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# --------------------------------------------------------------
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -6,7 +9,7 @@
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
@@ -14,26 +17,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+#
+# --------------------------------------------------------------
 
-import mdsclient
-from plugins.contracts import ICartridgeAgentPlugin
-from xml.dom.minidom import parse
-import socket
-from modules.util.log import LogFactory
-import time
-import subprocess
-import os
-
-class StormStartupNimbusHandler(ICartridgeAgentPlugin):
-
-    def run_plugin(self, values):
-        log = LogFactory().get_log(__name__)
-
-        # start server
-        log.info("Starting APACHE STORM NIMBUS...")
-
-        start_command = "${CARBON_HOME}/bin/storm nimbus"
-        env_var = os.environ.copy()
-        p = subprocess.Popen(start_command, env=env_var, shell=True)
-        output, errors = p.communicate()
-        log.debug("APACHE STORM NIMBUS started successfully")
+docker build -t apache/zookeeper:3.4.6 .
