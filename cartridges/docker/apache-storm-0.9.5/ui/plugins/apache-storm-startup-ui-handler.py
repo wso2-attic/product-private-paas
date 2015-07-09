6b05125cd61916ea4b36870075e54c099d837829
Jane Aarthy <jane@wso2.com>
2015-07-09 18:12:07 +0530

    adding apache-storm-nimbus docker catridge

---
+++ cartridges/docker/apache-storm-0.9.5/ui/plugins/apache-storm-startup-ui-handler.py
@@ -0,0 +1,46 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import mdsclient
+from plugins.contracts import ICartridgeAgentPlugin
+from xml.dom.minidom import parse
+import socket
+from modules.util.log import LogFactory
+import time
+import subprocess
+import os
+
+class StormStartupUIHandler(ICartridgeAgentPlugin):
+
+    def run_plugin(self, values):
+        log = LogFactory().get_log(__name__)
+        # configure server
+        log.info("Configuring APACHE STORM UI...")
+        config_command = "exec /opt/ppaas-configurator-4.1.0-SNAPSHOT/configurator.py"
+        env_var = os.environ.copy()
+        p = subprocess.Popen(config_command, env=env_var, shell=True)
+        output, errors = p.communicate()
+        log.info("APACHE STORM UI configured successfully")
+
+        # start server
+        log.info("Starting APACHE STORM UI...")
+
+        start_command = "${CARBON_HOME}/bin/storm ui"
+        env_var = os.environ.copy()
+        p = subprocess.Popen(start_command, env=env_var, shell=True)
+        output, errors = p.communicate()
+        log.debug("APACHE STORM UI started successfully")
