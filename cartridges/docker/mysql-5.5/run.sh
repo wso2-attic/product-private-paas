06882c3025fe63eb455f57af8338c0944f9da265
anuruddhal <anuruddha789@gmail.com>
2015-07-21 12:02:29 +0530

    Fixing run.sh

--- cartridges/docker/mysql-5.5/run.sh
+++ cartridges/docker/mysql-5.5/run.sh
@@ -12,4 +12,4 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=root123 -d -P mysql:5.5
+docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=root123 -d -P wso2/mysql:5.5
