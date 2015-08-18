b53d9139a5f0334133014498a031b7f7f9b7389f
anuruddhal <anuruddha789@gmail.com>
2015-08-18 16:57:45 +0530

    Adding new puppet modules

--- cartridges/vm/puppet/modules/stratos_base/manifests/init.pp
+++ cartridges/vm/puppet/modules/stratos_base/manifests/init.pp
@@ -1,23 +1,22 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-# 
-#   http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+# ----------------------------------------------------------------------------
+#  Copyright 2005-2015 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ----------------------------------------------------------------------------
 
 class stratos_base(
   $ensure = 'present',
-  $autoupgrade=true, 
+  $autoupgrade=true,
 ){
 
   if ! ($ensure in [ "present", "absent" ]) {
@@ -31,33 +30,34 @@ class stratos_base(
 # Set local variables based on the desired state
   if $ensure == "present" {
     if $autoupgrade == true {
-      $package_ensure = latest      
-    } 
-    else {
-      $package_ensure = present     
+      $package_ensure = latest
     }
-  } 
-  else {        
-      $package_ensure = absent      
+    else {
+      $package_ensure = present
+    }
+  }
+  else {
+    $package_ensure = absent
   }
 
   $packages = [
-    'nano',       
+    'nano',
     'curl',
-    'wget',    
+    'wget',
     'zip',
     'unzip',
+    'git',
     'tar']
 
   package { $packages:
-    ensure => $package_ensure,    
-  }  
+    ensure => $package_ensure,
+  }
 
   define printPackages{
-    notify { $name: 
+    notify { $name:
       message => "Installed package: ${name}",
     }
   }
-  printPackages{ $packages:}
+  printPackages{ $packages: }
 
 }
