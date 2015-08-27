40a58d24ff64fd5c4d7837ffef392ba1855017fe
reka <rthirunavukkarasu23@gmail.com>
2015-08-27 22:52:05 +0530

    defining the variables in the class body as a best practice

--- cartridges/vm/puppet/modules/apimanager/manifests/init.pp
+++ cartridges/vm/puppet/modules/apimanager/manifests/init.pp
@@ -17,17 +17,17 @@
 class apimanager (
   $server_name        = undef,
   $service_code       = 'apimanager',
-  $version          = undef,
+  $version            = undef,
   $owner              = 'root',
   $group              = 'root',
+)  {
+
   $target             = "/mnt/${server_ip}",
   $carbon_home        = "/mnt/${server_ip}/${server_name}-${version}",
   $configurator_home  = "/mnt/${configurator_name}-${configurator_version}",
   $template_module_pack = "${server_name}-${version}-template-module-${ppaas_version}.zip",
   $pca_home = "/mnt/${pca_name}-${pca_version}",
   $java_home  = "/opt/${java_name}"
-)  {
-
 # creating /mnt/{ip_address} folder
   exec {
     "creating_target_for_${server_name}":
--- cartridges/vm/puppet/modules/is/manifests/init.pp
+++ cartridges/vm/puppet/modules/is/manifests/init.pp
@@ -16,18 +16,18 @@
 
 class is (
   $server_name        = undef,
-  $version          = undef,
+  $version            = undef,
   $service_code       = 'is',
   $owner              = 'root',
   $group              = 'root',
+)  {
+
   $target             = "/mnt/${server_ip}",
   $carbon_home        = "/mnt/${server_ip}/${server_name}-${version}",
   $configurator_home  = "/mnt/${configurator_name}-${configurator_version}",
   $template_module_pack = "${server_name}-${version}-template-module-${ppaas_version}.zip",
   $pca_home = "/mnt/${pca_name}-${pca_version}",
   $java_home  = "/opt/${java_name}"
-
-)  {
 
 # creating /mnt/{ip_address} folder
   exec {
