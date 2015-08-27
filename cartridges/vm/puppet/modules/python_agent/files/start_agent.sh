5eb8153ea32141add2285cf21d91f5dfcddd2f9f
anuruddhal <anuruddha789@gmail.com>
2015-08-27 12:44:09 +0530

    Changing regex in vm basesd start_agent.sh script

--- cartridges/vm/puppet/modules/python_agent/files/start_agent.sh
+++ cartridges/vm/puppet/modules/python_agent/files/start_agent.sh
@@ -20,162 +20,116 @@ export APPLICATION_PATH="${CARBON_HOME}"
 echo "APPLICATION_PATH=${APPLICATION_PATH}" >> /etc/environment
 
 # mandatory parameters
-sed -i "s/MB-IP/${MB_IP}/g" ${PCA_HOME}/agent.conf
-sed -i "s/MB-PORT/${MB_PORT}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${MB_IP}" ]; then
+	sed -i "s/^.*mb.ip.*=.*$/mb.ip = ${MB_IP}/g" ${PCA_HOME}/agent.conf
+fi
 
+if [ ! -z "${MB_PORT}" ]; then
+	sed -i "s/^.*mb.port.*=.*$/mb.port = ${MB_PORT}/g" ${PCA_HOME}/agent.conf
+fi
 
 # parameters that can be empty
 # default values have to be set
 
-if [ -z "${LISTEN_ADDR}" ]; then
-	sed -i "s/LISTEN_ADDR/localhost/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/LISTEN_ADDR/${LISTEN_ADDR}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${LISTEN_ADDR}" ]; then
+	sed -i "s/^.*listen.address.*=.*$/listen.address = ${LISTEN_ADDR}/g" ${PCA_HOME}/agent.conf
 fi
 
 # defaults to the message broker IP if not set
-if [ -z "${CEP_IP}" ]; then
-	sed -i "s/CEP-IP/${MB_IP}/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/CEP-IP/${CEP_IP}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${CEP_IP}" ]; then
+	sed -i "s/^.*thrift.receiver.ip.*=.*$/thrift.receiver.ip  = ${CEP_IP}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${CEP_PORT}" ]; then
-	sed -i "s/CEP-PORT/7711/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/CEP-PORT/${CEP_PORT}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${CEP_PORT}" ]; then
+	sed -i "s/^.*thrift.receiver.port.*=.*$/thrift.receiver.port = ${CEP_PORT}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${CEP_USERNAME}" ]; then
-	sed -i "s/CEP-ADMIN-USERNAME/admin/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/CEP-ADMIN-USERNAME/${CEP_USERNAME}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${CEP_USERNAME}" ]; then
+	sed -i "s/^.*thrift.server.admin.username.*=.*$/thrift.server.admin.username = ${CEP_USERNAME}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${CEP_PASSWORD}" ]; then
-	sed -i "s/CEP-ADMIN-PASSWORD/admin/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/CEP-ADMIN-PASSWORD/${CEP_PASSWORD}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${CEP_PASSWORD}" ]; then
+	sed -i "s/^.*thrift.server.admin.password.*=.*$/thrift.server.admin.password = ${CEP_PASSWORD}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${ENABLE_HEALTH_PUBLISHER}" ]; then
-	sed -i "s/ENABLE_HEALTH_PUBLISHER/true/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/ENABLE_HEALTH_PUBLISHER/${ENABLE_HEALTH_PUBLISHER}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${ENABLE_HEALTH_PUBLISHER}" ]; then
+	sed -i "s/^.*cep.stats.publisher.enabled.*=.*$/cep.stats.publisher.enabled  = ${ENABLE_HEALTH_PUBLISHER}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${LB_PRIVATE_IP}" ]; then
-	sed -i "s/LB_PRIVATE_IP/ /g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/LB_PRIVATE_IP/${LB_PRIVATE_IP}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${LB_PRIVATE_IP}" ]; then
+	sed -i "s/^.*lb.private.ip.*=.*$/lb.private.ip =${LB_PRIVATE_IP} /g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${LB_PUBLIC_IP}" ]; then
-	sed -i "s/LB_PUBLIC_IP/ /g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/LB_PUBLIC_IP/${LB_PUBLIC_IP}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${LB_PUBLIC_IP}" ]; then
+	sed -i "s/^.*lb.public.ip.*=.*$/lb.public.ip = ${LB_PUBLIC_IP}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${ENABLE_ARTFCT_UPDATE}" ]; then
-	sed -i "s/ENABLE_ARTFCT_UPDATE/true/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/ENABLE_ARTFCT_UPDATE/${ENABLE_ARTFCT_UPDATE}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${ENABLE_ARTFCT_UPDATE}" ]; then
+	sed -i "s/^.*enable.artifact.update.*=.*$/enable.artifact.update = ${ENABLE_ARTFCT_UPDATE}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${COMMIT_ENABLED}" ]; then
-	sed -i "s/COMMIT_ENABLED/false/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/COMMIT_ENABLED/${COMMIT_ENABLED}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${COMMIT_ENABLED}" ]; then
+	sed -i "s/^.*auto.commit.*=.*$/auto.commit = ${COMMIT_ENABLED}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${CHECKOUT_ENABLED}" ]; then
-	sed -i "s/CHECKOUT_ENABLED/true/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/CHECKOUT_ENABLED/${CHECKOUT_ENABLED}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${CHECKOUT_ENABLED}" ]; then
+	sed -i "s/^.*auto.checkout.*=.*$/auto.checkout = ${CHECKOUT_ENABLED}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${ARTFCT_UPDATE_INT}" ]; then
-	sed -i "s/ARTFCT_UPDATE_INT/15/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/ARTFCT_UPDATE_INT/${ARTFCT_UPDATE_INT}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${ARTFCT_UPDATE_INT}" ]; then
+	sed -i "s/^.*artifact.update.interval.*=.*$/artifact.update.interval = ${ARTFCT_UPDATE_INT}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${ARTFCT_CLONE_RETRIES}" ]; then
-	sed -i "s/ARTFCT_CLONE_RETRIES/5/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/ARTFCT_CLONE_RETRIES/${ARTFCT_CLONE_RETRIES}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${ARTFCT_CLONE_RETRIES}" ]; then
+	sed -i "s/^.*artifact.clone.retries.*=.*$/artifact.clone.retries= ${ARTFCT_CLONE_RETRIES}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${ARTFCT_CLONE_INT}" ]; then
-	sed -i "s/ARTFCT_CLONE_INT/10/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/ARTFCT_CLONE_INT/${ARTFCT_CLONE_INT}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${ARTFCT_CLONE_INT}" ]; then
+	sed -i "s/^.*artifact.clone.interval.*=.*$/artifact.clone.interval= ${ARTFCT_CLONE_INT}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${PORT_CHECK_TIMEOUT}" ]; then
-	sed -i "s/PORT_CHECK_TIMEOUT/600000/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/PORT_CHECK_TIMEOUT/${PORT_CHECK_TIMEOUT}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${PORT_CHECK_TIMEOUT}" ]; then
+	sed -i "s/^.*port.check.timeout.*=.*$/port.check.timeout = ${PORT_CHECK_TIMEOUT}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${ENABLE_DATA_PUBLISHER}" ]; then
-	sed -i "s/ENABLE-DATA-PUBLISHER/false/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/ENABLE-DATA-PUBLISHER/${ENABLE_DATA_PUBLISHER}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${ENABLE_DATA_PUBLISHER}" ]; then
+	sed -i "s/^.*enable.data.publisher.*=.*$/enable.data.publisher  = ${ENABLE_DATA_PUBLISHER}/g" ${PCA_HOME}/agent.conf
 fi
 
 # defaults to the message broker IP if not set
-if [ -z "${MONITORING_SERVER_IP}" ]; then
-	sed -i "s/MONITORING-SERVER-IP/${MB_IP}/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/MONITORING-SERVER-IP/${MONITORING_SERVER_IP}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${MONITORING_SERVER_IP}" ]; then
+	sed -i "s/^.*monitoring.server.ip.*=.*$/monitoring.server.ip = ${MONITORING_SERVER_IP}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${MONITORING_SERVER_PORT}" ]; then
-	sed -i "s/MONITORING-SERVER-PORT/7611/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/MONITORING-SERVER-PORT/${MONITORING_SERVER_PORT}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${MONITORING_SERVER_PORT}" ]; then
+	sed -i "s/^.*monitoring.server.port.*=.*$/monitoring.server.port = ${MONITORING_SERVER_PORT}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${MONITORING_SERVER_SECURE_PORT}" ]; then
-	sed -i "s/MONITORING-SERVER-SECURE-PORT/7711/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/MONITORING-SERVER-SECURE-PORT/${MONITORING_SERVER_SECURE_PORT}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${MONITORING_SERVER_SECURE_PORT}" ]; then
+	sed -i "s/^.*monitoring.server.secure.port.*=.*$/monitoring.server.secure.port = ${MONITORING_SERVER_SECURE_PORT}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${MONITORING_SERVER_ADMIN_USERNAME}" ]; then
-	sed -i "s/MONITORING-SERVER-ADMIN-USERNAME/admin/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/MONITORING-SERVER-ADMIN-USERNAME/${MONITORING_SERVER_ADMIN_USERNAME}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${MONITORING_SERVER_ADMIN_USERNAME}" ]; then
+	sed -i "s/^.*monitoring.server.admin.username.*=.*$/monitoring.server.admin.username = ${MONITORING_SERVER_ADMIN_USERNAME}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${MONITORING_SERVER_ADMIN_PASSWORD}" ]; then
-	sed -i "s/MONITORING-SERVER-ADMIN-PASSWORD/admin/g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s/MONITORING-SERVER-ADMIN-PASSWORD/${MONITORING_SERVER_ADMIN_PASSWORD}/g" ${PCA_HOME}/agent.conf
+if [ ! -z "${MONITORING_SERVER_ADMIN_PASSWORD}" ]; then
+	sed -i "s/^.*monitoring.server.admin.password.*=.*$/monitoring.server.admin.password = ${MONITORING_SERVER_ADMIN_PASSWORD}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${LOG_FILE_PATHS}" ]; then
-	sed -i "s/LOG_FILE_PATHS/ /g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s#LOG_FILE_PATHS#${LOG_FILE_PATHS}#g" ${PCA_HOME}/agent.conf
+if [ ! -z "${LOG_FILE_PATHS}" ]; then
+	sed -i "s/^.*log.file.paths.*=.*$/log.file.paths = ${LOG_FILE_PATHS}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${APPLICATION_PATH}" ]; then
-	sed -i "s/APPLICATION-PATH/ /g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s#APPLICATION-PATH#${APPLICATION_PATH}#g" ${PCA_HOME}/agent.conf
+if [ ! -z "${APPLICATION_PATH}" ]; then
+	sed -i "s/^.*APPLICATION_PATH.*=.*$/APPLICATION_PATH = ${APPLICATION_PATH}/g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${METADATA_SERVICE_URL}" ]; then
-	sed -i "s/METADATA-SERVICE-URL/ /g" ${PCA_HOME}/agent.conf
-else
-	sed -i "s#METADATA-SERVICE-URL#${METADATA_SERVICE_URL}#g" ${PCA_HOME}/agent.conf
+if [ ! -z "${METADATA_SERVICE_URL}" ]; then
+	sed -i "s#^.*metadata.service.url.*=.*#metadata.service.url = ${METADATA_SERVICE_URL}#g" ${PCA_HOME}/agent.conf
 fi
 
-if [ -z "${LOG_LEVEL}" ]; then
-	sed -i "s/LOG_LEVEL/INFO/g" ${PCA_HOME}/logging.ini
-else
+if [ ! -z "${LOG_LEVEL}" ]; then
 	sed -i "s/LOG_LEVEL/${LOG_LEVEL}/g" ${PCA_HOME}/logging.ini
 fi
 
